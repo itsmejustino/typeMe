f46a79e615a4d8b5acbe3239b6640296f09fff5c
Justino <justindortiz@me.com>
2022-09-11 11:19:40 -0600

    dependencies

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const { InjectManifest, GenerateSW } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -18,10 +18,15 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new GenerateSW(),
       // html webpack plugin config
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'Text Editor',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
       }),
       // manifest file config. 
       new WebpackPwaManifest({
